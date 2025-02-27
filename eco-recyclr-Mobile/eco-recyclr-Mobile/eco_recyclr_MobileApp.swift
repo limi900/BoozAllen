710629492018f7d2c0fd53be120baa335dbc9dea
Jolomimeb <mebaghanjejolomi@gmail.com>
2025-02-26 20:45:40 -0500

    Creating mobile app

---
+++ eco-recyclr-Mobile/eco-recyclr-Mobile/eco_recyclr_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  eco_recyclr_MobileApp.swift
+//  eco-recyclr-Mobile
+//
+//  Created by Jolomi Mebaghanje on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct eco_recyclr_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
